e776e7caf2e1699a00db0d8cbdef9e72dfc19347
sreekayathris <103899037+sreekayathris@users.noreply.github.com>
2022-10-01 09:52:22 +0530

    Add files via upload

---
+++ assignment 2/David Rajasimman/load12.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+ds=pd.read_csv(r'C:\Users\Sree Kayathri\Desktop\Assignment 2/Churn_Modelling.csv')
+ds.shape
+ds.head()
